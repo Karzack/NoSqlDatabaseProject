b17b2055fc734758bf0e1f01903e3698b863a64c
Ola Dahl <oladahl.lel@gmail.com>
2018-05-14 17:08:08 +0200

    Add helpers for sample data

--- mongodb/src/beavercoffee/helpers/AddIngridientsHelper.java
+++ mongodb/src/beavercoffee/helpers/AddIngridientsHelper.java
@@ -1,4 +1,28 @@
 package beavercoffee.helpers;
 
+import beavercoffee.models.product.Ingridient;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class AddIngridientsHelper {
+
+    public static List<Ingridient> addIngridients() {
+
+//        return Arrays.asList(
+//                new Ingridient("Espresso Roast", "g"),
+//                new Ingridient("Whole Bean Roast", "g"),
+//                new Ingridient("Whole Bean Light Roast", "g"),
+//                new Ingridient("Cocoa Mix", "g"),
+//                new Ingridient("Skim Milk", "l"),
+//                new Ingridient("Soy Milk", "l"),
+//                new Ingridient("Whole Milk", "l"),
+//                new Ingridient("2% Milk", "l"),
+//                new Ingridient("Whipped Cream", "l"),
+//                new Ingridient("Vanilla Syrup", "l"),
+//                new Ingridient("Caramel Syrup", "l"),
+//                new Ingridient("Irish Cream", "l")
+//        );
+        return null;
+    }
 }
--- mongodb/src/beavercoffee/helpers/AddProductsHelper.java
+++ mongodb/src/beavercoffee/helpers/AddProductsHelper.java
@@ -1,4 +1,77 @@
 package beavercoffee.helpers;
 
+import beavercoffee.DBInstance;
+import beavercoffee.models.Unit;
+import beavercoffee.models.product.*;
+import com.mongodb.client.MongoCollection;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class AddProductsHelper {
+
+    public static void addProducts() {
+        MongoCollection<Product> collection = DBInstance.getInstance().getCollection("products", Product.class);
+
+        collection.drop(); //TODO Inte bra lösning, fixa.
+
+        List<Product> products = Arrays.asList(
+                new Product(
+                        new ProductName("Bönkaffe Espresso Rostning", "Espresso Roast"),
+                        new ProductPrice(15.0, 1.1),
+                        Arrays.asList(
+                                new IngridientItem(new Ingridient("Espresso Roast", Unit.GRAM), 500)
+                        )),
+                new Product(
+                        new ProductName("Bönkaffe Fransk Rostning", "Whole Bean French Roast"),
+                        new ProductPrice(80, 8),
+                        Arrays.asList(
+                                new IngridientItem(new Ingridient("Whole Bean French Roast", Unit.GRAM), 500)
+                        )),
+                new Product(
+                        new ProductName("Bönkaffe Ljus Rostning", "Whole Bean Light Roast"),
+                        new ProductPrice(80, 8),
+                        Arrays.asList(
+                                new IngridientItem(new Ingridient("Whole Bean Light Roast", Unit.GRAM), 500)
+                        )),
+                new Product(
+                        new ProductName("Espresso", "Espresso"),
+                        new ProductPrice(20, 1.2),
+                        Arrays.asList(
+                                new IngridientItem(new Ingridient("Espresso Roast", Unit.GRAM), 20)
+                        )),
+                new Product(
+                        new ProductName("Kaffe med mjölk", "Coffee with milk"),
+                        new ProductPrice(15, 1),
+                        Arrays.asList(
+                                new IngridientItem(new Ingridient("Whole Bean Light Roast", Unit.GRAM), 20),
+                                new IngridientItem(new Ingridient("Milk", Unit.LITRE), 0.2)
+                        ))
+        );
+
+        collection.insertMany(products);
+    }
+
+    public void testListProducts() {
+        MongoCollection<Product> collection = DBInstance.getInstance().getCollection("products", Product.class);
+        for (Product product : collection.find()) {
+            System.out.println(product.getName().getEnglish() + "\nContains:");
+            for (IngridientItem item : product.getIngridients()) {
+                switch (item.getIngridient().getUnit()) {
+                    case GRAM:
+                        System.out.println(item.getIngridient().getName() + ": " + item.getQuantity() + " grams");
+                        break;
+                    case LITRE:
+                        System.out.println(item.getIngridient().getName() + ": " + item.getQuantity() + " litres");
+                        break;
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+
+
+
 }
