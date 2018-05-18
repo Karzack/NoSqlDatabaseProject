19cf37e3066d6542271e852d428925090e5fcc0e
OmarAldulaimi <omar.aldulaimi@hotmail.com>
2018-05-18 20:13:06 +0200

    Fixat login

--- mongodb/src/beavercoffee/GUI/MainPanel.java
+++ mongodb/src/beavercoffee/GUI/MainPanel.java
@@ -5,19 +5,26 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
+
 public class MainPanel extends JPanel {
-    private JPanel menuPanel = new JPanel();
-    private JPanel menuLeft = new JPanel();
-    private JPanel menuRight = new JPanel();
+
     private int height, width;
+    private JLabel label = new JLabel("Welcome");
+    private JLabel label2 = new JLabel("Location");
+    private JLabel label3 = new JLabel("Rolls");
+    private JLabel label4 = new JLabel("SSN");
+    private String[] location = new String[] {"Malmö", "Stockholm", "Chicago"};
+    private String[] rolls = new String[] {"Employee", "Employer", "Manager"};
+    private JComboBox comboBox = new JComboBox(location);
+    private JComboBox rule = new JComboBox(rolls);
     private Button addCustomer = new Button("Add custommer");
     private Button addEmplyee = new Button("Add employee");
-    private Button orders = new Button("orders");
-    private Button addOrder = new Button("Add order");
+    private Button login = new Button("Log in");
+    private JTextField textField = new JTextField();
     private JFrame frame = new JFrame();
     private JFrame frame2 = new JFrame();
     private JFrame frame3 = new JFrame();
-    private JFrame frame4 = new JFrame();
+
 
     public MainPanel(int height, int width) {
         this.height = height;
@@ -27,16 +34,30 @@ public class MainPanel extends JPanel {
 
     private void initGUI() {
         setPreferredSize(new Dimension(height, width));
-        setLayout(new GridLayout(0,3));
-        JLabel lblMenu = new JLabel("Welcome!", SwingConstants.CENTER);
-        add(menuLeft);
-        add(menuPanel);
-        add(menuRight);
-        menuPanel.add(lblMenu);
-        menuLeft.add(addCustomer);
-        menuLeft.add(addEmplyee);
-        menuLeft.add(orders);
-        menuLeft.add(addOrder);
+        this.setLayout(null);
+        this.add(label);
+        this.add(label2);
+        this.add(label3);
+        this.add(label4);
+        this.add(comboBox);
+        this.add(textField);
+        this.add(rule);
+        this.add(login);
+        this.add(addEmplyee);
+        this.add(addCustomer);
+        label.setBounds(180, 30, 80, 40);
+        label2.setBounds(50, 50, 80, 40);
+        label3.setBounds(50, 80, 80, 40);
+        label4.setBounds(50, 120, 80, 40);
+        comboBox.setBounds(120, 60, 180, 30);
+        rule.setBounds(120, 90, 180, 30);
+        textField.setBounds(120, 120, 180, 30);
+        login.setBounds(150, 150, 120, 30);
+        addEmplyee.setBounds(50, 180, 140, 30);
+        addCustomer.setBounds(210, 180, 140, 30);
+
+
+
         addCustomer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -55,27 +76,14 @@ public class MainPanel extends JPanel {
 
             }
         });
-        orders.addActionListener(new ActionListener() {
+        login.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                frame3.getContentPane().add(new Orders(500,400));
-                frame3.pack();
-                frame3.setVisible(true);
-
+                
             }
         });
-        addOrder.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                frame4.getContentPane().add(new AddOrder(500,400));
-                frame4.pack();
-                frame4.setVisible(true);
 
-            }
-        });
     }
 
-    public JPanel getMenuPanel() {
-        return menuPanel;
-    }
+
 }
