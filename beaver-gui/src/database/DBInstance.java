c85f7b3c0ce8270d94d0f0b9c346c3b302d67575
Ola Dahl <oladahl.lel@gmail.com>
2018-05-30 17:25:04 +0200

    Simplify connection to database

--- beaver-gui/src/database/DBInstance.java
+++ beaver-gui/src/database/DBInstance.java
@@ -9,34 +9,31 @@ import org.bson.codecs.pojo.PojoCodecProvider;
 import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
 import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
+/**
+ * @author Ola Dahl
+ */
 public class DBInstance {
     private static MongoClient instance = null;
+    private static MongoDatabase db = null;
     private static final String DB_NAME = "BeaverCoffeeDB";
 
-    protected DBInstance() {}
-
-    public static MongoClient getInstance() {
-        if (instance == null) {
-            CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
-                    fromProviders(PojoCodecProvider.builder().automatic(true).build()));
-            MongoClient client = new MongoClient("localhost",
-                    MongoClientOptions.builder().codecRegistry(pojoCodecRegistry).build());
-
-            instance = client;
-//            instance = client.getDatabase(DB_NAME);
-        }
-        return instance;
+    protected DBInstance() {
     }
 
     public static MongoDatabase connectDB() {
-        getInstance();
-        return instance.getDatabase(DB_NAME);
+        if (db == null) {
+            CodecRegistry pojoCodecRegistry = fromRegistries(MongoClient.getDefaultCodecRegistry(),
+                    fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+            instance = new MongoClient("localhost",
+                    MongoClientOptions.builder().codecRegistry(pojoCodecRegistry).build());
+            db = instance.getDatabase(DB_NAME);
+        }
+
+        return db;
     }
 
     public static void disconnectDB() {
-        getInstance();
-        instance.close();
+        if (instance != null)
+            instance.close();
     }
-
-
 }
